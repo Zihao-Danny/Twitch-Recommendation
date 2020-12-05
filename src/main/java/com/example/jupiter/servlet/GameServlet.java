74a7dd20c55ae397a4cd20c76b2db456f0845653
wang_zihao <zihaow.danny@gmail.com>
2020-12-04 20:11:38 -0800

    first try

---
+++ src/main/java/com/example/jupiter/servlet/GameServlet.java
@@ -0,0 +1,56 @@
+package com.example.jupiter.servlet;
+
+import com.example.jupiter.entity.Game;
+import com.example.jupiter.external.TwitchClient;
+import com.example.jupiter.external.TwitchException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "GameServlet", urlPatterns = {"/game"})
+public class GameServlet extends HttpServlet {
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+//        String name = jsonRequest.getString("name");
+//        String developer = jsonRequest.getString("developer");
+//        String releaseTime = jsonRequest.getString("release_time");
+//        String website = jsonRequest.getString("website");
+//        float price = jsonRequest.getFloat("price");
+//
+//        System.out.println("Name is: " + name);
+//        System.out.println("Developer is: " + developer);
+//        System.out.println("Release time is: " + releaseTime);
+//        System.out.println("Website is: " + website);
+//        System.out.println("Price is: " + price);
+//
+//        response.setContentType("application/json");
+//        JSONObject jsonResponse = new JSONObject();
+//        jsonResponse.put("status", "ok");
+//        response.getWriter().print(jsonResponse);
+//
+//    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String gameName = request.getParameter("game_name");
+        TwitchClient client = new TwitchClient();
+
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
+
+    }
+}
